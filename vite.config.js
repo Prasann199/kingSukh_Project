a474d71ff1ade2e045bdb9f31e712ff939b71900
Prasann199 <prasannmalanaik@gmail.com>
2024-08-16 20:16:26 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/InnoByteServices_Project/"
+  base:"/KingSukh_GuestHouse_Project/"
 
 })
