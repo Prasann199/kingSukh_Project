157de5b5a8faa4ce752772721c2e19e1814cb666
Prasann199 <prasannmalanaik@gmail.com>
2024-08-19 13:03:32 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/KingSukh_GuestHouse_Project/",
+  base:"/kingSukh_Project/",
   build: {
     outDir: 'dist', // Ensure the output directory is correct
     rollupOptions: {
